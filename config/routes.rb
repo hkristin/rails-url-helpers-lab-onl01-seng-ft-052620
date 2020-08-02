6a06b5760fdffa46bf4a8a0e82cb0002e5d3b15b
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-02 23:25:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show]
-  get '/students/:id/activate', to: 'students#activate', as: 'active'
+  get '/students/:id/activate_student_path', to: 'students#activate_student_path', as: 'active'
 end
