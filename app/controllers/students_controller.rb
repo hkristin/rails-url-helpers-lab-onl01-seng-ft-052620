6e684aa313fe7c7632a705f35bc137a6e6ba8786
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-02 23:21:29 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,21 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+
   end
 
+  def activate
+
+  end
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
